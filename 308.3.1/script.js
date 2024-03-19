20c0e98612f1ac153a4d40432b8db8980be8fc3c
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-03-19 17:37:22 -0600

    updated the part 3 feeling loopy

--- 308.3.1/script.js
+++ 308.3.1/script.js
@@ -5,20 +5,20 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log(`For number ${i}: Fizz`);
-  }
-  if (i % 5 === 0) {
-    console.log(`For number ${i}: Buzz`);
-  }
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`For number ${i} Fizz Buzz`);
-  }
-  if (i % 3 != 0 && i % 5 != 0) {
-    console.log(`For number ${i} Not divisible by 3 or 5 `);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0) {
+//     console.log(`For number ${i}: Fizz`);
+//   }
+//   if (i % 5 === 0) {
+//     console.log(`For number ${i}: Buzz`);
+//   }
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(`For number ${i} Fizz Buzz`);
+//   }
+//   if (i % 3 != 0 && i % 5 != 0) {
+//     console.log(`For number ${i} Not divisible by 3 or 5 `);
+//   }
+// }
 
 // Part 2: Prime Time
 // Declare an arbitrary number, n.
@@ -64,23 +64,45 @@ for (let i = 1; i <= 100; i++) {
 
 //Part 3:Feeling Loopy
 
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+// You can make the following assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than “\n”.
+
 const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+let cellBuilder = "";
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let rowCounter = 0;
+
 for (const c of csvString) {
-    let cell1 = "";
-    if (c != ",") {
-        cell1 += c
-        console.log(cell1);
-    } else {
-        break;
+  if (c != ",") {
+    cellBuilder += c;
+  } else {
+    if (rowCounter == 0) {
+      cell1 = cellBuilder;
+    } else if (rowCounter == 1) {
+      cell2 = cellBuilder;
+    } else if (rowCounter == 2) {
+      cell3 = cellBuilder;
     }
 
-
-    let cell2 = "";
-    let cell3 = "";
-    let cell4 = "";
-
+    rowCounter++;
+    cellBuilder = "";
+  }
+  
+  if (c == "\n") {
+    cell4 = cellBuilder;
+    console.log(cell1, cell2, cell3, cell4);
+    rowCounter = 0;
+  }
 }
-
-
-
-
+console.log(cell1, cell2, cell3, cell4);
